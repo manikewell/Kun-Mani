1fec84402ced1d616e9d79eee408a5b162b2a5d2
manikewell <manikewell@gmail.com>
2012-06-23 18:16:24 +0800

    2nd commit
    Test Application

---
+++ GitTest/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitTest.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is <b>HelloWorld</b>...";
+        } 
+
+    }
+}
